f0ed89540f7da03faebe2b55c88975d83ae5c37d
Sarah Paz <sarah.paz@gmail.com>
2019-11-04 23:44:07 -0500

    add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,20 @@
 #users
 sarah = User.create(username: 'sarah', password: 'test')
 jane = User.create(username: 'jane', password: 'test')
+dee = User.create(username: 'dee', password: 'test')
 
 #pets
 sarah.pets.create(name: 'Kahlua', age: 15, image: 'https://robohash.org/1?set=set4&size=200x200', cause: 'Lymphoma', goal: 1500)
 
 jane.pets.create(name: 'Dolores', age: 6, image: 'https://robohash.org/2?set=set4&size=200x200', cause: 'Stage 1 kidney disease', goal: 500)
 
+dee.pets.create([{name: 'Bones', age: 5, image: 'https://robohash.org/3?set=set4&size=200x200', cause: 'Heart disease', goal: 300}, {name: 'Ginger', age: 11, image: 'https://robohash.org/4?set=set4&size=200x200', cause: 'Liver disease', goal: 700}])
+
+sarah.pets.create(name: 'Thumper', age: 2, image: 'https://robohash.org/5?set=set4&size=200x200', cause: 'Diabetes', goal: 250)
+jane.pets.create(name: 'Tango', age: 4, image: 'https://robohash.org/6?set=set4&size=200x200', cause: 'Stage 3 kidney disease', goal: 250)
+
 #donations
 Donation.create(amount: 100, message: 'Best wishes!', user_id: 1, pet_id: 2)
 Donation.create(amount: 200, message: 'Sending so much love!', user_id: 2, pet_id: 1)
+Donation.create(amount: 200, message: 'Stay positive, we love you!', user_id: 3, pet_id: 1)
+Donation.create(amount: 200, message: 'I hope this helps, get well soon!', user_id: 1, pet_id: 5)
