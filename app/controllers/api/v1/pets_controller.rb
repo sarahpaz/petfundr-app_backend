24af308dd6aabb710fff57076b86ee3f94254489
Sarah Paz <sarah.paz@gmail.com>
2019-11-04 20:40:17 -0500

    create api / v1 folder and move controllers

---
+++ app/controllers/api/v1/pets_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::PetsController < ApplicationController
+	def index
+		pets = Pet.all
+		render json: pets
+	end
+
+	def show
+		pet = Pet.find_by(id: params[:id])
+		render json: pet
+	end
+end
