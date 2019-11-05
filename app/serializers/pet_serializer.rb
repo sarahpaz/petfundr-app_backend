6d868da2218a323f9fbb5aef3c96553862b21388
Sarah Paz <sarah.paz@gmail.com>
2019-11-04 20:39:39 -0500

    add associations to serializer

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,3 +1,6 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :image, :cause, :goal
+	attributes :id, :name, :age, :image, :cause, :goal
+	
+	belongs_to :user
+	has_many :donations
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password
+	attributes :id, :username
+	
+	has_many :pets
+	has_many :donations
 end
