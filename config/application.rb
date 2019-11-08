0d4ac9747367006c146a1d1e103a6fef47ab843b
Sarah Paz <sarah.paz@gmail.com>
2019-11-07 23:54:53 -0500

    add config middleware to grab cookies and sessions

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module PetfundrAppBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+		config.api_only = true
+		#* config middleware to grab cookies and sessions
+		config.middleware.use ActionDispatch::Cookies
+		config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name'
   end
 end
