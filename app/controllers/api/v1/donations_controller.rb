57bbcdfbe0f787ac37c931407acaa122b2544663
Sarah Paz <sarah.paz@gmail.com>
2019-11-07 23:52:10 -0500

    add index and show actions

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::DonationsController < ApplicationController
 	def index
-		donations = Donation.all
-		render json: donations
+		@donations = Donation.all
+		render json: @donations
 	end
 
 	def show
-		donation = Donation.find_by(id: params[:id])
-		render json: donation
+		@donation = Donation.find_by(id: params[:id])
+		render json: @donation
 	end
 end
