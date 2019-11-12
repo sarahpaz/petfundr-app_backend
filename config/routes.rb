a6957346a8f7a0528029657e744d7f161c56accf
Sarah Paz <sarah.paz@gmail.com>
2019-11-11 19:14:37 -0500

    add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	post 'api/v1/login' => 'api/v1/sessions#create'
 	delete 'api/v1/logout' => 'api/v1/sessions#destroy'
 	get 'api/v1/get_current_user' => 'api/v1/sessions#get_current_user'
-
+	post 'api/v1/signup' => 'api/v1/users#create'
 
 	namespace :api do
 		namespace :v1 do
