1f4e0c3dfd0baa9a24909e94d17446e9ef4f0120
Sarah Paz <sarah.paz@gmail.com>
2019-11-11 19:14:06 -0500

    add password to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	has_many :pets
 	has_many :donations
 
-	validates :username, presence: true
+	validates :username, :password, presence: true
 	validates :username, uniqueness: true
 
 	has_secure_password
