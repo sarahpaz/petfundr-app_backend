47ff5dcdc9ad514ccc54d1fc9e25727fce94a53f
Sarah Paz <sarah.paz@gmail.com>
2019-11-15 21:55:53 -0500

    add post route for donations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	delete 'api/v1/logout' => 'api/v1/sessions#destroy'
 	get 'api/v1/get_current_user' => 'api/v1/sessions#get_current_user'
 	post 'api/v1/signup' => 'api/v1/users#create'
+	post 'api/v1/donations' => 'api/v1/donations#create'
+
 	
 	namespace :api do
 		namespace :v1 do
