9398597dd0b1a13d0d309b4d933fdcdf95a660d3
Sarah Paz <sarah.paz@gmail.com>
2019-11-15 19:54:34 -0500

    add pet_id and user_id

--- app/serializers/donation_serializer.rb
+++ app/serializers/donation_serializer.rb
@@ -1,4 +1,16 @@
 class DonationSerializer
   include FastJsonapi::ObjectSerializer
 	attributes :amount, :message
+
+	attribute :pet do |donation|
+		{
+			id: donation.pet_id,
+		}
+	end
+
+	attribute :user do |donation|
+			{
+				id: donation.user_id,
+			}
+		end
 end
